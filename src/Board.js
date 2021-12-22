6b03e37822cb728f1a3d7fb12923341a0ebae515
LaTonya Johnson <latonya.johnson0921@gmail.com>
2021-12-21 18:18:09 -0600

    3x3

--- src/Board.js
+++ src/Board.js
@@ -27,7 +27,7 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.5 }) {
+function Board({ nrows = 3, ncols = 3, chanceLightStartsOn = 0.5 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
